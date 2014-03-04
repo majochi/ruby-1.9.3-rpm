761c6c05e8dc669e8728cf364e97c089a08ad669
JKN <knust@shd-online.de>
2014-03-04 10:19:31 +0100

    add ARCH handling

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.gz
 cd ~/rpmbuild/SPECS
 curl https://raw.github.com/lnxchk/ruby-1.9.3-rpm/master/ruby193.spec > ruby193.spec
 rpmbuild -bb ruby193.spec
-rpm -Uvh ~/rpmbuild/RPMS/x86_64/ruby-1.9.3p0-1.${ARCH}.rpm
+ARCH=$(uname -i)
+rpm -Uvh ~/rpmbuild/RPMS/$ARCH/ruby-1.9.3p0-1.$ARCH.rpm
