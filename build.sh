cd422562660c5d068fbc4473d15591c390003972
JKN <knust@shd-online.de>
2014-03-04 10:33:52 +0100

    minor

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 yum install -y rpm-build rpmdevtools libyaml-devel
 rpmdev-setuptree
 cd ~/rpmbuild/SOURCES
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.gz
+curl -O http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.gz
 cd ~/rpmbuild/SPECS
 curl https://raw.github.com/lnxchk/ruby-1.9.3-rpm/master/ruby193.spec > ruby193.spec
 rpmbuild -bb ruby193.spec
